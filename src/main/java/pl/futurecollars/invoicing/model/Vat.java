599d27cee3774f6b38a4e6d26cf088b2a2a0480d
Anna Kulbicka <ania.kulbicka@onet.pl>
2023-03-14 15:01:21 +0100

    Init Changes

---
+++ src/main/java/pl/futurecollars/invoicing/model/Vat.java
@@ -0,0 +1,24 @@
+package pl.futurecollars.invoicing.model;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+
+public enum Vat {
+
+  VAT_23(23),
+  VAT_8(8),
+  VAT_7(7),
+  VAT_5(5),
+  VAT_0(0),
+  VAT_ZW(0);
+
+  private final BigDecimal rate;
+
+  Vat(int rate) {
+    this.rate = BigDecimal.valueOf(rate);
+  }
+}
--- src/test/groovy/pl/futurecollars/invoicing/TestHelpers.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/TestHelpers.groovy
@@ -11,13 +11,13 @@ class TestHelpers {
 
     static company(int id) {
         new Company(("$id").repeat(10),
-                "ul. Bukowińska 24d/$id 02-703 Warszawa, Polska",
-                "iCode Trust $id Sp. z o.o")
+            "ul. Bukowińska 24d/$id 02-703 Warszawa, Polska",
+            "iCode Trust $id Sp. z o.o")
     }
 
     static product(int id) {
         new InvoiceEntry("Programming course $id", BigDecimal.valueOf(id * 1000),
-                BigDecimal.valueOf(id * 1000 * 0.08), Vat.VAT_8)
+            BigDecimal.valueOf(id * 1000 * 0.08), Vat.VAT_8)
     }
 
     static invoice(int id) {
