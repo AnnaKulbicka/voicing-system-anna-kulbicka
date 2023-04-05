7b8651d8cf0315f88be5f2b5ed3844e7b067265e
Anna Kulbicka <ania.kulbicka@onet.pl>
2023-04-05 21:09:17 +0200

    Commit

---
+++ config/Config.java
@@ -0,0 +1,8 @@
+package pl.futurecollars.invoicing.config;
+
+@SuppressWarnings("unused")
+public class Config {
+
+  public static final String DATABASE_LOCATION = "db/invoices.txt";
+  public static final String ID_FILE_LOCATION = "db/id.txt";
+}
